c8032311031a37f15f9643506f75caaf26843308
Micah Britton <micahwkb@gmail.com>
2016-11-02 11:56:24 -0400

    build 3 working functions within countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,57 @@
+// best method, takes console input
+
+var string = process.argv.slice(2);
+
+if (!string[0]) console.log("Please provide some text!");
+else countLetters(string);
+
+function countLetters(string) {
+  var countStats = new Object();
+  string.join("").split("").forEach(function(char) {
+    if (countStats[char]) countStats[char]++;
+    else countStats[char] = 1;
+  });
+  console.log(countStats);
+}
+
+// console.log(countLetters(string));
+
+// improved version of below, a bit cleaner, fewer variables etc
+/*
+
+function countLetters(string) {
+  var countStats = new Object();
+  string.replace(/\s/, ""); // remove spaces
+  for (var i = 0; i < string.length; i++) {
+    var letter = string.charAt(i);
+    if (countStats[letter]) countStats[letter] += 1;
+    else countStats[letter] = 1;
+  }
+  return countStats;
+}
+
+console.log(countLetters("testy test"));
+console.log(countLetters("lighthouse's in the house lighthouse in the house"));
+
+*/
+
+
+// first attempt, works well but doesn't accept console input
+/*
+
+function countLetters(string) {
+  var countStats = new Object();
+  var concatStr = string.split(" ").join(""); // remove spaces
+  // return concatStr;
+  // return typeof concatStr;
+  for (var i = 0; i < concatStr.length; i++) {
+    var letter = concatStr.charAt(i);
+    if (countStats[letter]) countStats[letter] += 1;
+    else countStats[letter] = 1;
+  }
+  return countStats;
+}
+
+console.log(countLetters("testy test"));
+console.log(countLetters("lighthouse in the house lighthouse in the house"));
+*/
